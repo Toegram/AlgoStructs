2c19652ace5fb87b5e6c3a543c69f572fa7c8935
Tony <TTuretsky@gmail.com>
2019-01-17 14:26:44 -0500

    reversed num kept sign

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversedStrN = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+
+  const sign = Math.sign(n);
+
+  return parseInt(reversedStrN) * sign;
+}
+reverseInt(69);
 
 module.exports = reverseInt;
