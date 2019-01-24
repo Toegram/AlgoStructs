12de2341cd75b25c0e348b2a6e12b0a8a9f2f852
Tony <TTuretsky@gmail.com>
2019-01-24 13:35:11 -0500

    checked anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,36 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const objA = buildCharMap(stringA);
+  const objB = buildCharMap(stringB);
+
+  if (Object.keys(objA).length !== Object.keys(objB).length) {
+    return false;
+  }
+
+  for (let char in objA) {
+    if (objA[char] !== objB[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function buildCharMap(str) {
+  const strReplace = str.replace(/[^\w]/g, "").toLowerCase();
+  const obj = {};
+
+  for (let char of strReplace) {
+    !obj[char] ? (obj[char] = 1) : obj[char]++;
+  }
+
+  return obj;
+}
+
+// //REGEX to get rid of all white spaces and characters
+// const strA = stringA.replace(/[^\w]/g, "").toLowerCase();
+// const strB = stringB.replace(/[^\w]/g, "").toLowerCase();
 
 module.exports = anagrams;
